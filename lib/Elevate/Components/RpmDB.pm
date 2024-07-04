98a690e45f6fca99cb0bde05d4f53484ca027ca2
Dmitry Shibut <dshibut@cloudlinux.com>
2024-07-04 14:12:55 +0400

    Change the removal of obsolete packages to use rpm -e --nodeps

--- lib/Elevate/Components/RpmDB.pm
+++ lib/Elevate/Components/RpmDB.pm
@@ -57,11 +57,12 @@ sub _cleanup_rpms ($self) {
 }
 
 sub _remove_obsolete_packages ($self) {
-
-    return if $self->getopt('skip-removing-obsolete-packages');
-
     my @pkgs_to_remove = OBSOLETE_PACKAGES();
-    $self->yum->remove(@pkgs_to_remove);
+
+    foreach my $pkg (@pkgs_to_remove) {
+        $self->rpm->remove_no_dependencies_and_justdb($pkg);
+    }
+
     return;
 }
 
